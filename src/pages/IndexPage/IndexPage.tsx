f243472f58296205949a3642efb37780b16794c5
Bogdan Rozhkov <lomus.br@gmail.com>
2025-09-14 20:21:07 +0300

    commit

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,4 +1,4 @@
-import {Section, Cell, Image, List, Tabbar} from '@telegram-apps/telegram-ui';
+import {Section, Cell, Image, List} from '@telegram-apps/telegram-ui';
 import type {FC} from 'react';
 
 import {Link} from '@/components/Link/Link.tsx';
