6154c8379da9e9db7ab4502c22f4f7d56eb6cad0
Bogdan Rozhkov <lomus.br@gmail.com>
2025-12-16 04:59:40 +0300

    some commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,7 @@
 import {useSignal, miniApp, useLaunchParams} from '@tma.js/sdk-react';
 import {AppRoot} from '@telegram-apps/telegram-ui';
 
-import {createBrowserRouter} from "react-router";
-import {RouterProvider} from "react-router/dom";
+import {Route, Routes} from "react-router";
 import {
   InitDataPage,
   ThemeParamsPage,
@@ -43,39 +42,31 @@ export function App() {
       appearance={isDark ? 'dark' : 'light'}
       platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
     >
-      <RouterProvider router={router}/>
+      <Routes>
+        <Route path="/" element={<PageWithTabbar/>}>
+          {import.meta.env.DEV && <Route path='/init-data' element={<InitDataPage/>}/>},
+          {import.meta.env.DEV && <Route path='/theme-params' element={<ThemeParamsPage/>}/>},
+          {import.meta.env.DEV && <Route path='/launch-params' element={<LaunchParamsPage/>}/>},
+
+          <Route index={true} element={<WishlistsPage/>}/>,
+          <Route path='/wishlists/new' element={<NewWishlistPage/>}/>,
+          <Route path='/wishlists/new' element={<NewWishlistPage/>}/>,
+          <Route path='/wishlist/:wishlistId/items' element={<WishlistItemsPage/>}/>,
+          <Route path='/wishlist/:wishlistId/edit' element={<EditWishlistPage/>}/>,
+          <Route path='/wishlist/:wishlistId/items/new' element={<NewWishlistItemPage/>}/>,
+          <Route path='/wishlist/:wishlistId/item/:itemId' element={<WishlistItemPage/>}/>,
+          <Route path='/wishlist/:wishlistId/item/:itemId/edit' element={<EditWishlistItemPage/>}/>,
+
+          <Route path='/friends' element={<FriendsPage/>}/>,
+          <Route path='/friend/new' element={<NewFriendPage/>}/>,
+          <Route path='/friends/requests/incoming' element={<IncomingFriendsRequestsPage/>}/>,
+          <Route path='/friend/:friendId/wishlists' element={<FriendWishlistsPage/>}/>,
+          <Route path='/friend/:friendId/wishlist/:wishlistId/items' element={<FriendWishlistItemsPage/>}/>,
+          <Route path='/friend/:friendId/wishlist/:wishlistId/item/:itemId' element={<FriendWishlistItemPage/>}/>,
+
+          <Route path='/settings' element={<SettingsPage/>}/>
+        </Route>
+      </Routes>
     </AppRoot>
   );
 }
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: PageWithTabbar,
-    errorElement: <ErrorBoundary/>,
-    children:
-      [
-        {path: '/init-data', Component: InitDataPage},
-        {path: '/theme-params', Component: ThemeParamsPage},
-        {path: '/launch-params', Component: LaunchParamsPage},
-
-        {index: true, Component: WishlistsPage},
-        {path: '/wishlists/new', Component: NewWishlistPage},
-        {path: '/wishlists/new', Component: NewWishlistPage},
-        {path: '/wishlist/:wishlistId/items', Component: WishlistItemsPage},
-        {path: '/wishlist/:wishlistId/edit', Component: EditWishlistPage},
-        {path: '/wishlist/:wishlistId/items/new', Component: NewWishlistItemPage},
-        {path: '/wishlist/:wishlistId/item/:itemId', Component: WishlistItemPage},
-        {path: '/wishlist/:wishlistId/item/:itemId/edit', Component: EditWishlistItemPage},
-
-        {path: '/friends', Component: FriendsPage},
-        {path: '/friend/new', Component: NewFriendPage},
-        {path: '/friends/requests/incoming', Component: IncomingFriendsRequestsPage},
-        {path: '/friend/:friendId/wishlists', Component: FriendWishlistsPage},
-        {path: '/friend/:friendId/wishlist/:wishlistId/items', Component: FriendWishlistItemsPage},
-        {path: '/friend/:friendId/wishlist/:wishlistId/item/:itemId', Component: FriendWishlistItemPage},
-
-        {path: '/settings', Component: SettingsPage}
-      ]
-  },
-]);
